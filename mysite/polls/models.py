03bb64f5780ac73b559ab3d71f2dda0f9fafeb47
ivan_grigorev <rauscral@gmail.com>
2020-06-05 12:59:34 +0300

    end FBV

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,11 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField('Вопрос', max_length=200)
+    pub_date = models.DateTimeField('Дата публикации')
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -15,8 +14,8 @@ class Question(models.Model):
         return self.question_text
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, verbose_name='Вопрос', on_delete=models.CASCADE)
+    choice_text = models.CharField('Ответ', max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
