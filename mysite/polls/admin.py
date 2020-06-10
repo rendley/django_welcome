1aff87976f9379959562ac8d4292842e3e1d2028
ivan_grigorev <rauscral@gmail.com>
2020-06-10 16:13:16 +0300

    admin customize

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,31 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+# добавление полей Foreign Key в другую модель 
+class Choiceline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+# class Choiceline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # порядок полей в админке
+    # fields = ['pub_date', 'question_text']
+
+    # группировка полей по вертикали
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    # создание редактирования ответов на вопросы в админке
+    inlines = [Choiceline]
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
